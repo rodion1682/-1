d317b4b2542cd878d818b4e86b937977ce0875e2
rodion1682 <rodiontropkin17@gmail.com>
2023-04-26 14:27:01 +0300

    26.04

--- src/app/hooks/useAuth.jsx
+++ src/app/hooks/useAuth.jsx
@@ -1,12 +1,170 @@
+//import React, { useContext, useState, useEffect } from "react";
+//import PropTypes from "prop-types";
+//import { toast } from "react-toastify";
+//import axios from "axios";
+//import userService from "../services/user.service";
+//import localStorageService, {
+//    setTokens
+//} from "../services/localStorage.service";
+//import { useHistory } from "react-router-dom";
+
+//export const httpAuth = axios.create({
+//    baseURL: "https://identitytoolkit.googleapis.com/v1/",
+//    params: {
+//        key: process.env.REACT_APP_FIREBASE_KEY
+//    }
+//});
+//const AuthContext = React.createContext();
+
+//export const useAuth = () => {
+//    return useContext(AuthContext);
+//};
+
+//const AuthProvider = ({ children }) => {
+//    const [currentUser, setUser] = useState();
+//    const [error, setError] = useState(null);
+//    const [isLoading, setLoading] = useState(true);
+//    const history = useHistory();
+
+//    async function logIn({ email, password }) {
+//        try {
+//            const { data } = await httpAuth.post(
+//                `accounts:signInWithPassword`,
+//                {
+//                    email,
+//                    password,
+//                    returnSecureToken: true
+//                }
+//            );
+//            setTokens(data);
+//            await getUserData();
+//        } catch (error) {
+//            errorCatcher(error);
+//            const { code, message } = error.response.data.error;
+//            console.log(code, message);
+//            if (code === 400) {
+//                switch (message) {
+//                    case "INVALID_PASSWORD":
+//                        throw new Error("Email или пароль введены некорректно");
+
+//                    default:
+//                        throw new Error(
+//                            "Слишком много попыток входа. Попробуйте позже"
+//                        );
+//                }
+//            }
+//        }
+//    }
+//    function logOut() {
+//        localStorageService.removeAuthData();
+//        setUser(null);
+//        history.push("/");
+//    }
+//    function randomInt(min, max) {
+//        return Math.floor(Math.random() * (max - min + 1) + min);
+//    }
+//    async function signUp({ email, password, ...rest }) {
+//        try {
+//            const { data } = await httpAuth.post(`accounts:signUp`, {
+//                email,
+//                password,
+//                returnSecureToken: true
+//            });
+//            setTokens(data);
+//            await createUser({
+//                _id: data.localId,
+//                email,
+//                rate: randomInt(1, 5),
+//                completedMeetings: randomInt(0, 200),
+//                image: `https://avatars.dicebear.com/api/avataaars/${(
+//                    Math.random() + 1
+//                )
+//                    .toString(36)
+//                    .substring(7)}.svg`,
+//                ...rest
+//            });
+//        } catch (error) {
+//            errorCatcher(error);
+//            const { code, message } = error.response.data.error;
+//            console.log(code, message);
+//            if (code === 400) {
+//                if (message === "EMAIL_EXISTS") {
+//                    const errorObject = {
+//                        email: "Пользователь с таким Email уже существует"
+//                    };
+//                    throw errorObject;
+//                }
+//            }
+//        }
+//    }
+//    async function createUser(data) {
+//        try {
+//            const { content } = await userService.create(data);
+//            console.log(content);
+//            setUser(content);
+//        } catch (error) {
+//            errorCatcher(error);
+//        }
+//    }
+//    function errorCatcher(error) {
+//        const { message } = error.response.data;
+//        setError(message);
+//    }
+//    async function getUserData() {
+//        try {
+//            const { content } = await userService.getCurrentUser();
+//            setUser(content);
+//        } catch (error) {
+//            errorCatcher(error);
+//        } finally {
+//            setLoading(false);
+//        }
+//    }
+//    useEffect(() => {
+//        if (localStorageService.getAccessToken()) {
+//            getUserData();
+//        } else {
+//            setLoading(false);
+//        }
+//    }, []);
+//    useEffect(() => {
+//        if (error !== null) {
+//            toast(error);
+//            setError(null);
+//        }
+//    }, [error]);
+//    return (
+//        <AuthContext.Provider value={{ signUp, logIn, currentUser, logOut }}>
+//            {!isLoading ? children : "Loading..."}
+//        </AuthContext.Provider>
+//    );
+//};
+
+//AuthProvider.propTypes = {
+//    children: PropTypes.oneOfType([
+//        PropTypes.arrayOf(PropTypes.node),
+//        PropTypes.node
+//    ])
+//};
+
+//export default AuthProvider;
+
 import React, { useContext, useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
+import { toast } from 'react-toastify';
 import axios from 'axios';
 import userService from '../services/user.service';
-import { toast } from 'react-toastify';
-import { setTokens } from '../services/localsStorage.service';
+import localStorageService, {
+	setTokens,
+} from '../services/localStorage.service';
+import { useHistory } from 'react-router-dom';
 
-const httpAuth = axios.create();
-
+export const httpAuth = axios.create({
+	baseURL: 'https://identitytoolkit.googleapis.com/v1/',
+	params: {
+		key: process.env.REACT_APP_FIREBASE_KEY,
+	},
+});
 const AuthContext = React.createContext();
 
 export const useAuth = () => {
@@ -14,20 +172,19 @@ export const useAuth = () => {
 };
 
 const AuthProvider = ({ children }) => {
-	const [currentUser, setUser] = useState({});
+	const [currentUser, setUser] = useState();
 	const [error, setError] = useState(null);
+	const [isLoading, setLoading] = useState(true);
+	const history = useHistory();
+
 	async function logIn({ email, password }) {
 		try {
-			const { data } = await httpAuth.post(
-				`https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${process.env.REACT_APP_FIREBASE_KEY}`,
-				{
-					email,
-					password,
-					returnSecureToken: true,
-				}
-			);
+			const { data } = await httpAuth.post(`accounts:signInWithPassword`, {
+				email,
+				password,
+				returnSecureToken: true,
+			});
 			setTokens(data);
-			console.log(data);
 			await getUserData();
 		} catch (error) {
 			errorCatcher(error);
@@ -46,50 +203,86 @@ const AuthProvider = ({ children }) => {
 			}
 		}
 	}
-	async function getUserData() {
+	function logOut() {
+		localStorageService.removeAuthData();
+		setUser(null);
+		history.push('/');
+	}
+	function randomInt(min, max) {
+		return Math.floor(Math.random() * (max - min + 1) + min);
+	}
+	async function updateUserData(data) {
 		try {
-			const { content } = await userService.getCurrentUser();
+			const { content } = await userService.update(data);
 			setUser(content);
 		} catch (error) {
 			errorCatcher(error);
 		}
 	}
-
-	async function singUp({ email, password, ...rest }) {
-		const url = `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${process.env.REACT_APP_FIREBASE_KEY}`;
+	async function signUp({ email, password, ...rest }) {
 		try {
-			const { data } = await httpAuth.post(url, {
+			const { data } = await httpAuth.post(`accounts:signUp`, {
 				email,
 				password,
 				returnSecureToken: true,
 			});
 			setTokens(data);
-			await createUser({ _id: data.localId, email, ...rest });
-		} catch (error) {
-			errorCatcher(error);
-		}
-	}
-	async function createUser(data) {
-		try {
-			const { content } = userService.create(data);
-			setUser(content);
+			await createUser({
+				_id: data.localId,
+				email,
+				rate: randomInt(1, 5),
+				completedMeetings: randomInt(0, 200),
+				image: `https://avatars.dicebear.com/api/avataaars/${(
+					Math.random() + 1
+				)
+					.toString(36)
+					.substring(7)}.svg`,
+				...rest,
+			});
 		} catch (error) {
 			errorCatcher(error);
 			const { code, message } = error.response.data.error;
+			console.log(code, message);
 			if (code === 400) {
 				if (message === 'EMAIL_EXISTS') {
 					const errorObject = {
-						email: 'Пользователь с таким email уже существует',
+						email: 'Пользователь с таким Email уже существует',
 					};
 					throw errorObject;
 				}
 			}
 		}
 	}
+	async function createUser(data) {
+		try {
+			const { content } = await userService.create(data);
+			console.log(content);
+			setUser(content);
+		} catch (error) {
+			errorCatcher(error);
+		}
+	}
 	function errorCatcher(error) {
 		const { message } = error.response.data;
 		setError(message);
 	}
+	async function getUserData() {
+		try {
+			const { content } = await userService.getCurrentUser();
+			setUser(content);
+		} catch (error) {
+			errorCatcher(error);
+		} finally {
+			setLoading(false);
+		}
+	}
+	useEffect(() => {
+		if (localStorageService.getAccessToken()) {
+			getUserData();
+		} else {
+			setLoading(false);
+		}
+	}, []);
 	useEffect(() => {
 		if (error !== null) {
 			toast(error);
@@ -97,8 +290,10 @@ const AuthProvider = ({ children }) => {
 		}
 	}, [error]);
 	return (
-		<AuthContext.Provider value={{ singUp, currentUser, logIn }}>
-			{children}
+		<AuthContext.Provider
+			value={{ signUp, logIn, currentUser, logOut, updateUserData }}
+		>
+			{!isLoading ? children : 'Loading...'}
 		</AuthContext.Provider>
 	);
 };
--- src/app/services/user.service.js
+++ src/app/services/user.service.js
@@ -1,24 +1,25 @@
-import httpService from './http.service';
-import localsStorageService from './localsStorage.service';
-const userEndpoint = 'user/';
+import httpService from "./http.service";
+import localStorageService from "./localStorage.service";
+
+const userEndpoint = "user/";
 
 const userService = {
-	get: async () => {
-		const { data } = await httpService.get(userEndpoint);
-		return data;
-	},
-	create: async (payload) => {
-		const { data } = await httpService.put(
-			userEndpoint + payload._id,
-			payload
-		);
-		return data;
-	},
-	getCurrentUser: async () => {
-		const { data } = await httpService.get(
-			userEndpoint + localsStorageService.getUserId()
-		);
-		return data;
-	},
+    get: async () => {
+        const { data } = await httpService.get(userEndpoint);
+        return data;
+    },
+    create: async (payload) => {
+        const { data } = await httpService.put(
+            userEndpoint + payload._id,
+            payload
+        );
+        return data;
+    },
+    getCurrentUser: async () => {
+        const { data } = await httpService.get(
+            userEndpoint + localStorageService.getUserId()
+        );
+        return data;
+    }
 };
 export default userService;
